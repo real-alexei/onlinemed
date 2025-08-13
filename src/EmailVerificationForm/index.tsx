e0840e4265b243743de634e7948ea4a13c1cbbb8
Alexei Selivanov <alexei.selivanov@gmail.com>
2025-08-13 12:14:08 +0300

    fixes

--- src/EmailVerificationForm/index.tsx
+++ src/EmailVerificationForm/index.tsx
@@ -4,16 +4,13 @@ import Input from '../Input'
 import CodeInput from '../CodeInput'
 import styles from './index.module.css'
 
-export default function EmailVerificationForm({ onSubmit }: EmailVerificationFormProps) {
+export default function EmailVerificationForm({ onSubmit }) {
   const [email, setEmail] = useState('')
   const [isLoading, setIsLoading] = useState(false)
   const [isEmailVerified, setIsEmailVerified] = useState(false)
 
   return (
     <form
-      className={classNames(styles.container, {
-        [styles.expanded]: isEmailVerified
-      })}
       onSubmit={(e) => {
         e.preventDefault()
         setIsLoading(true)
@@ -25,33 +22,37 @@ export default function EmailVerificationForm({ onSubmit }: EmailVerificationFor
         }, 2000)
       }}
     >
-      <Input
-        label='Email'
-        placeholder='Enter your email'
-        type='email'
-        defaultValue={email}
-        onChange={(e) => setEmail(e.target.value)}
-        loading={isLoading}
-        verified={isEmailVerified}
-        onReset={() => {
-          setEmail('')
-          setIsEmailVerified(false)
-          setIsLoading(false)
-        }}
-      />
-
-      <div className={classNames(styles.codeInput, {
+      <div className={classNames(styles.container, {
         [styles.expanded]: isEmailVerified
       })}>
-        <div className={styles.codeInputContent}>
-          <div><strong>Enter verification code</strong></div>
-          <div>Enter the code sent to {email} to use your saved information.</div>
+        <Input
+          label='Email'
+          placeholder='Enter your email'
+          type='email'
+          defaultValue={email}
+          onChange={(e) => setEmail(e.target.value)}
+          loading={isLoading}
+          verified={isEmailVerified}
+          onReset={() => {
+            setEmail('')
+            setIsEmailVerified(false)
+            setIsLoading(false)
+          }}
+        />
 
-          <CodeInput style={{ marginTop: '1em', marginBottom: '0.5em' }} />
+        <div className={classNames(styles.codeInput, {
+          [styles.expanded]: isEmailVerified
+        })}>
+          <div className={styles.codeInputContent}>
+            <div><strong>Enter verification code</strong></div>
+            <div>Enter the code sent to {email} to use your saved information.</div>
 
-          <small>
-            Didn't receive a code? <a href="#">Send again</a>
-          </small>
+            <CodeInput style={{ marginTop: '1em', marginBottom: '0.5em' }} />
+
+            <small>
+              Didn't receive a code? <a href="#">Send again</a>
+            </small>
+          </div>
         </div>
       </div>
     </form>
